4a67a49b9c3b4bcecb711b0137aa789ce6b2efb5
David Nanjila <nanjiladavid2@gmail.com>
2024-04-30 15:58:06 +0300

    Feat: add a js script that fetches the api data and sends to the html page

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const apiKey = 'your API key here'
+const apiUrl = 'your url here'
+//The above information is omitted due to privacy reasons but the app works well with the api key and url provided by openweathermap.org
+const locationInput = document.querySelector('#locationInput')
+const  searchButton = document.querySelector('#searchButton')
+const  locationElement = document.querySelector('#location')
+const  temperatureElement = document.querySelector('#temperature')
+const descriptionElement = document.querySelector('#description')
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value
+    if(location){
+        fetchWeather(location);
+    }else{
+        alert('Please enter a location')
+    }
+   
+});
+function fetchWeather(location){
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        locationElement.textContent = data.name
+        temperatureElement.textContent = `${Math.round(data.main.temp)}°C`
+        descriptionElement.textContent = data.weather[0].description
+    })
+    .catch(() => {
+       console.log('An error occurred')
+    });
+}
